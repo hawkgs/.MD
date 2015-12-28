40100696948a4be8e3f315374e9e60174fe80e50
hAWKdv <hawk94@abv.bg>
2015-12-28 12:54:22 +0200

    Added initial Ng project files

--- web/public/ng_app/app/app.cmp.ts
+++ web/public/ng_app/app/app.cmp.ts
@@ -2,21 +2,17 @@ import {Component, ViewEncapsulation} from "angular2/core";
 import {RouteConfig, ROUTER_DIRECTIVES} from "angular2/router";
 
 // Components
-import {HomeCmp} from "../components/home/home";
-import {AboutCmp} from "../components/about/about";
-import {NameList} from "../services/name_list";
 
 // Main Component
 @Component({
     selector: "app",
-    viewProviders: [NameList],
     templateUrl: "./app/app.html",
     styleUrls: ["./app/app.css"],
     encapsulation: ViewEncapsulation.None,
     directives: [ROUTER_DIRECTIVES]
 })
 @RouteConfig([
-    { path: "/", component: HomeCmp, as: "Home" },
-    { path: "/about", component: AboutCmp, as: "About" }
+/*    { path: "/", component: HomeCmp, as: "Home" },
+    { path: "/about", component: AboutCmp, as: "About" }*/
 ])
-export class AppCmp {}
+export class AppComponent {}
