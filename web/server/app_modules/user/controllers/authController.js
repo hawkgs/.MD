8cb83ad3f624d5e90beab59ffae8b487e6141231
hAWKdv <hawk94@abv.bg>
2016-02-08 00:07:09 +0200

    Fixed user authentication

--- web/server/app_modules/user/controllers/authController.js
+++ web/server/app_modules/user/controllers/authController.js
@@ -3,38 +3,36 @@
 var passport = require("passport"),
     jwt = require("jsonwebtoken"),
     SECRET = "#yoloMojeBi #sweg trolololo lel",
-    TOKEN_EXP_MIN = 5;
+    TOKEN_EXP_MIN = 3; // todo change mate
 
 var AuthController = {
     login: function (req, res, next) {
-        console.log('attempting login...');
-        //res.status(200);
+        var auth = passport.authenticate("local", function (err, user) {
+            var token;
 
-        return res.send({tolotolo: "trololo" });
-        //
-        //var auth = passport.authenticate("local", function (err, user) {
-        //    console.log(user);
-        //
-        //    if (err) {
-        //        return next(err);
-        //    }
-        //
-        //    if (!user) {
-        //        res.send({ success: false });
-        //    }
-        //
-        //    req.logIn(user, function (err) {
-        //        if (err) {
-        //            return next(err);
-        //        }
-        //
-        //        // The username will be sent inside the token
-        //        user.token = AuthController._generateToken({ username: user.username });
-        //        res.send({ success: true, user: user, token: user.token });
-        //    });
-        //});
-        //
-        //auth(req, res, next);
+            if (err) {
+                return next(err);
+            }
+
+            // If the user doesn't exist
+            if (!user) {
+                res.status(400);
+                res.send({ success: false });
+            }
+
+            req.logIn(user, function (err) {
+                if (err) {
+                    return next(err);
+                }
+
+                token = AuthController._generateToken({ username: user.username });
+
+                res.status(200);
+                res.send({ success: true, username: user.username, token: token });
+            });
+        });
+
+        auth(req, res, next);
     },
 
     logout: function (req, res) {
@@ -52,7 +50,7 @@ var AuthController = {
     },
 
     _generateToken: function (payload) {
-        return jwt.sign(payload, SECRET, { expiresInMinutes: 60 * TOKEN_EXP_MIN });
+        return jwt.sign(payload, SECRET, { expiresIn: 60 * TOKEN_EXP_MIN });
     }
 };
 
